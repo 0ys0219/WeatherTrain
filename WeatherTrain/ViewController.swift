d5835800747e37a64fd6d0e60f91c113d0598b4a
Ethan.0ys <lmt860219@gmail.com>
2023-09-09 21:58:04 +0800

    Initial Commit

---
+++ WeatherTrain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTrain
+//
+//  Created by 林育生 on 2023/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
